08fddc71e25ea93fd18b4d72555600b6ffa2e5eb
Yosef Margolin <yukele@hotmail.com>
2017-11-02 14:16:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,9 +9,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   }
 
 function addElementToEndOfArray(array, element) {
-  return [array,...element]
+  return [array, ...element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
   return array.push(element)
 }
+
