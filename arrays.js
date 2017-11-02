6472ef73caa40d8afe8b1beae140e6663336cc13
Yosef Margolin <yukele@hotmail.com>
2017-11-02 14:57:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,12 +29,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
-  
+
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
-
-
